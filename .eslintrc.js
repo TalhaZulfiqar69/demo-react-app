4aa7a88a7a2b92f1c2552224fdcc913f4a0319e4
Muhammad Talha  Zulfiqar <talharana69@yahoo.com>
2021-02-12 00:36:28 +0500

    husky implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-typescript/base',
-    ],
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
